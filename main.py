25af99ebeeccd1636dabecdcbb306e6959ecb7f2
ZinganDovgaluk <SoftMark228@gmail.com>
2021-06-06 21:48:13 +0300

    DropBox Auth

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import dropbox
 from dropbox import Dropbox
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, session
+from flask.sessions import SessionInterface
 from flask_sqlalchemy import SQLAlchemy
 from dropbox import DropboxOAuth2FlowNoRedirect
 
 from DropBoxAPI import DbxApi
 
 app = Flask(__name__)
-
+app.secret_key = 'super secret key'
+app.config['SESSION_TYPE'] = 'filesystem'
 # app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite///DataBase.db'
 # db = SQLAlchemy(app)
 
@@ -16,21 +18,16 @@ app = Flask(__name__)
 # class User(db.Model):
 
 
-@app.route('/')
-@app.route('/home')
 def index():
     return render_template("index.html", auth_link=DbxApi._authorize_url)
-
-
-@app.route('/test1')
-@app.route('/test2')
-def test():
-    return "Test page"
 
 
 @app.route('/user')
 def user(auth_code):
     account = DbxApi.get_account(auth_code)
+    if account is None:
+        flash("Wrong auth code!")
+        return index()
     return account.email
 
 
@@ -41,7 +38,8 @@ def login():
     if request.method == 'POST':
         auth_code = request.form["auth_code"]
         if auth_code != "":
-            return redirect(url_for("user", auth_code=auth_code))
+            return user(auth_code=auth_code)
+        else: flash("Field is empty!")
 
 #  elif request.method == 'GET':
     #flash('You must be logged in DropBox in current browser!')
