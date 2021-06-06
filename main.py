cefe319eddbc978b76a43d23d6605497f29881cf
ZinganDovgaluk <SoftMark228@gmail.com>
2021-06-06 08:38:37 +0300

    Hello GET and POST methods

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite///DataBase.db'
 db = SQLAlchemy(app)
+
+# User
+
+# class User(db.Model):
 
 
 @app.route('/')
@@ -19,9 +23,18 @@ def test():
     return "Test page"
 
 
-@app.route('/user/<string:login>/<int:id>')
-def user(login, id):
-    return f"| Login: {login} | ID: {str(id) } |"
+@app.route('/user/<string:mail>/')
+def user(mail):
+    return f"| MAIL: {mail} |"
+
+
+@app.route('/login', methods=['post', 'get'])
+def login():
+    if request.method == 'POST':
+        email = request.form["mail"]
+        return redirect(url_for("user", mail=email))
+    elif request.method == 'GET':
+        return render_template('index.html')
 
 
 if __name__ == "__main__":
---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+
+    def get_file(self):
+        pass
+
+    def put_file(self):
+        pass
+
